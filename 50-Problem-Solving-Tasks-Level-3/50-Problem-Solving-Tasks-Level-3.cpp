d700a190aaeba3f479b3e3c5f5f43c4d061d6d84
Emy1pa <souaouti.iman@gmail.com>
2025-07-10 17:55:09 +0100

    Solve Problem 8: Multiply two 3x3 matrices with random numbers and display the result.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -4,40 +4,40 @@
 
 using namespace std;
 
+int RandomNumber(int From, int To){
+	int Random = rand() % (To - From + 1);
+	return Random;
+}
 
-void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols) {
-	int Counter = 0;
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols) {
 	for (int i = 0; i < Rows; i++)
 	{
 		for (int j = 0; j < Cols; j++) {
-			Counter++;
-			arr[i][j] = Counter;
+			arr[i][j] = RandomNumber(1, 100);
 		}
 	}
 }
 
-void PrintMatrix(int arr[3][3], short Rows, short Cols){
-	cout << "The following is a 3x3 Ordered matrix: \n";
+void PrintMatrix(int arr[3][3], short Rows, short Cols) {
 	for (int i = 0; i < Rows; i++)
 	{
 		for (int j = 0; j < Cols; j++) {
-			cout << arr[i][j] << "\t";
+			cout << setw(3) << arr[i][j] << "\t";
 		}
 		cout << endl;
 	}
 }
 
-void PrintTransposedMatrix(int arr[3][3], int Rows, int Cols) {
-	cout << "the following is the transposed matrix: \n";
-	for (int j = 0; j < Cols; j++)
+void PrintMultiplyOfTwoMatrix(int arr1[3][3], int arr2[3][3], short Rows, short Cols) {
+	for (int i = 0; i < Rows; i++)
 	{
-		for (int i = 0; i < Rows; i++)
-		{
-			cout << arr[i][j] << "\t";
+		for (int j = 0; j < Cols; j++) {
+			arr1[i][j] *= arr2[i][j];
 		}
-		cout << endl;
+		
 	}
 }
+
 
 
 
@@ -45,13 +45,25 @@ int main()
 {
 	srand((unsigned)time(NULL));
 	
-	int arr[3][3];
+	int arr1[3][3], arr2[3][3];
 
-	FillMatrixWithOrderedNumbers(arr, 3, 3);
+	FillMatrixWithRandomNumbers(arr1, 3, 3);
+	FillMatrixWithRandomNumbers(arr2, 3, 3);
 
-	PrintMatrix(arr, 3, 3);
+	cout << "Matrix 1: \n";
+	PrintMatrix(arr1, 3, 3);
 
-	PrintTransposedMatrix(arr, 3, 3);
+	cout << endl;
+
+	cout << "Matrix 2: \n";
+	PrintMatrix(arr2, 3, 3);
+
+	PrintMultiplyOfTwoMatrix(arr1, arr2, 3, 3);
+
+	cout << "Results: \n";
+	PrintMatrix(arr1,  3, 3);
+	
+
 	system("pause>0");
 }
 
