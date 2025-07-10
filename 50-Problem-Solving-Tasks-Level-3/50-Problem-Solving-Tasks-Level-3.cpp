fc35608767895edc1fff795c1807001203cd62e1
Emy1pa <souaouti.iman@gmail.com>
2025-07-10 17:37:22 +0100

    Solve Problem 7: Fill 3x3 matrix, display it, then display its transposed version correctly.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include <cstdlib>
 #include <iomanip>
+
 using namespace std;
 
-void fillMatrixWithOrderedNumbers(int arr[3][3], int Rows, int Cols, int &Counter) {
-	Counter = 1;
+
+void FillMatrixWithOrderedNumbers(int arr[3][3], short Rows, short Cols) {
+	int Counter = 0;
 	for (int i = 0; i < Rows; i++)
 	{
 		for (int j = 0; j < Cols; j++) {
-			arr[i][j] = Counter;
 			Counter++;
+			arr[i][j] = Counter;
 		}
 	}
-
 }
 
-void PrintMatrixOrderedNumbers(int arr[3][3], int rows, int cols) {
-	cout << "The following is a 3x3 ordered matrix: \n";
-
-	for (int i = 0; i < rows; i++)
+void PrintMatrix(int arr[3][3], short Rows, short Cols){
+	cout << "The following is a 3x3 Ordered matrix: \n";
+	for (int i = 0; i < Rows; i++)
 	{
-		for (int j = 0; j < cols; j++)
+		for (int j = 0; j < Cols; j++) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+void PrintTransposedMatrix(int arr[3][3], int Rows, int Cols) {
+	cout << "the following is the transposed matrix: \n";
+	for (int j = 0; j < Cols; j++)
+	{
+		for (int i = 0; i < Rows; i++)
 		{
 			cout << arr[i][j] << "\t";
 		}
@@ -29,15 +40,18 @@ void PrintMatrixOrderedNumbers(int arr[3][3], int rows, int cols) {
 }
 
 
+
 int main()
 {
+	srand((unsigned)time(NULL));
+	
 	int arr[3][3];
-	int Counter = 0;
 
-	fillMatrixWithOrderedNumbers(arr, 3, 3, Counter);
+	FillMatrixWithOrderedNumbers(arr, 3, 3);
 
-	PrintMatrixOrderedNumbers(arr, 3, 3);
+	PrintMatrix(arr, 3, 3);
 
-    system("pause>0");
+	PrintTransposedMatrix(arr, 3, 3);
+	system("pause>0");
 }
 
