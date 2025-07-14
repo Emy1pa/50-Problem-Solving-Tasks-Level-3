485dd7374b53e45061d54da36ab01621ee4970ec
Emy1pa <souaouti.iman@gmail.com>
2025-07-14 18:19:37 +0100

    Solve Problem 18: add function to find and print intersected numbers between two 3x3 matrices

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -18,43 +18,63 @@ void PrintMatrix(int arr[3][3], short Rows, short Cols)
 	}
 }
 
-bool IsNumberExists(int arr[3][3], int Number, short Rows, short Cols) {
-	for (short i = 0; i < Rows; i++)
+bool CheckNumberIfExistsInMatrix(int arr[3][3], int Number, short Rows, short Cols) {
+	for (int i = 0; i < Rows; i++)
 	{
-		for (short j = 0; j < Cols; j++)
-		{
-			if (arr[i][j] == Number) {
+		for (int j = 0; j < Cols; j++) {
+			if (arr[i][j] == Number)
 				return true;
-			}
 		}
 	}
 	return false;
-	
 }
+
+void IntersectedNumbers(int arr1[3][3], int arr2[3][3], short Rows, short Cols, int arrIntersect[9], int &arrIntLength) {
+	arrIntLength = 0;
+	for (int i = 0; i < Rows; i++)
+	{
+		for (int j = 0; j < Cols; j++) {
+			if (CheckNumberIfExistsInMatrix(arr2, arr1[i][j], Rows, Cols)) {
+				arrIntersect[arrIntLength] = arr1[i][j];
+				arrIntLength++;
+			}
+		}
+	}
+}
+
+void PrintIntersectedNumbers(int arrIntersect[9], int arrIntLength) {
+	for (int i = 0; i < arrIntLength; i++)
+	{
+		cout << arrIntersect[i] << "\t";
+	}
+}
+
 
 
 
 int main()
 {
 
-	int arr[3][3] = { {77, 5, 12}, {22, 20, 1}, {1, 0, 9} };
+	int arr1[3][3] = { {77, 5, 12}, {22, 20, 1}, {1, 0, 9} };
+	int arr2[3][3] = { {5, 80, 90}, {22, 77, 1}, {10, 8, 33} };
+
+	int arrIntersect[9] = {};
+	int arrIntLength = 0;
 
 	cout << "Matrix 1: \n";
-	PrintMatrix(arr, 3, 3);
+	PrintMatrix(arr1, 3, 3);
 	
-	int Number = 0;
-
-	cout << "Please enter the number to look for in matrix ? : ";
-	cin >> Number;
 	cout << endl;
 
-	if (IsNumberExists(arr, Number, 3, 3)) {
-		cout << "Yes it is there.";
-	}
-	else {
-		cout << "No it is not there.";
-	}
+	cout << "Matrix 2: \n";
+	PrintMatrix(arr2, 3, 3);
+
+	IntersectedNumbers(arr1, arr2, 3, 3, arrIntersect, arrIntLength);
+
 	cout << endl;
+
+	cout << "Intersected Numbers are: \n";
+	PrintIntersectedNumbers(arrIntersect, arrIntLength);
 
 	system("pause>0");
 }
