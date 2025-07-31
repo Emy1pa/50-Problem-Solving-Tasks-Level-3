64cf8f904b3fca8a74f238e4903dd16df219385c
Emy1pa <souaouti.iman@gmail.com>
2025-07-31 14:12:17 +0100

    Solve Problem 40: Add overloaded JoinString for vector and array with delimiter handling logic.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -14,6 +14,16 @@ string JoinString(vector <string> vString, string Delim){
 	return JoinStrings.substr(0, JoinStrings.length() - Delim.length());
 }
 
+string JoinString(string arrString[], short Length, string Delim) {
+	string JoinStrings = "";
+
+	for (int i = 0; i < Length; i++)
+	{
+		JoinStrings = JoinStrings + arrString[i]+ Delim;
+	}
+	return JoinStrings.substr(0, JoinStrings.length() - Delim.length());
+}
+
 
 
 
@@ -24,9 +34,14 @@ int main()
 {
 	
 	vector <string> vString = { "Mohammed", "Faid", "Ali", "Maher" };
+	string arrString[] = { "Mohammed", "Faid", "Ali", "Maher" };
 	cout << "\nVector after join: \n";
 
 	cout << JoinString(vString, " ");
+
+	cout << "\nArray after join: \n";
+
+	cout << JoinString(arrString, 4, " ");
 
 	system("pause>0");
 }
