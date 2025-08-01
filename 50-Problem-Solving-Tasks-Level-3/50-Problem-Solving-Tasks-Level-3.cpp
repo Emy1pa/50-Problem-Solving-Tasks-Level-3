7e6177b846209857d756020e124fed904940fa51
Emy1pa <souaouti.iman@gmail.com>
2025-08-01 17:46:05 +0100

    Fix Problem 44:  remove punctuation correctly from string without skipping characters during iteration

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -4,91 +4,28 @@
 
 using namespace std;
 
-
-vector <string> SplitString(string S1, string Delim) {
-	short pos = 0;
-	vector <string> vString;
-	string sWord;
-	while ((pos = S1.find(Delim)) != std::string::npos) {
-		sWord = S1.substr(0, pos);
-		if (sWord != "") {
-			vString.push_back(sWord);
-		}
-		S1.erase(0, pos + Delim.length());
-	}
-	if (S1 != "") {
-		vString.push_back(S1);
-	}
-	return vString;
-}
-
-
-
-vector <string> ReplaceStringWithMatchCase(string S1, string StringToReplace, string ReplaceTo){
-		vector <string> S2 = SplitString(S1, " ");
-		for (int i = 0; i < S2.size(); i++)
-		{
-			if (S2[i] == StringToReplace) {
-				S2[i] = ReplaceTo;
-			}
-	}
-	
-	return S2;
-}
-
-bool CheckIfMatchCase(string S1, string S2) {
-	for (int i = 0; i < S2.size(); i++)
+string RemovePunctuation(string S1){
+	string Result = "";
+	for (int i = 0; i < S1.length();)
 	{
-		if (tolower(S1[i]) != tolower(S2[i])) {
-			return false;
+		if (ispunct(S1[i])) {
+			S1.erase(i, 1);
+		}
+		else {
+			i++;
 		}
 	}
-
-	return true;
-}
-
-vector <string> ReplaceStringWithNoMatchCase(string S1, string StringToReplace, string ReplaceTo) {
-	vector <string> S2 = SplitString(S1, " ");
-	for (int i = 0; i < S2.size(); i++)
-	{
-		if (CheckIfMatchCase(S2[i], StringToReplace)) {
-			S2[i] = ReplaceTo;
-		}
-	}
-
-	return S2;
-}
-
-
-                        
-string JoinString(vector <string> vString, string Delim){
-	string S1 = "";
-	for (string& s : vString) {
-		S1 = S1 + s + Delim;
-	}
-	return S1.substr(0, S1.length() - Delim.length());
+	return S1;
 }
 
 
 int main()
 {
-	string S1 = "Welcome To Jordan , Jordan is a nice country";
-
-	string StringToReplace = "Jordan";
-	string ReplaceTo = "USA";
-
-	cout << "\n Original String \n" << S1 ;
-
-	vector <string> S3 = ReplaceStringWithMatchCase(S1, StringToReplace, ReplaceTo);
-	string ResultMatch = JoinString(S3, " ");
-	cout << "\n\n Replace with Match Case: \n";
-	cout << ResultMatch << endl;
-
+	string S1 = "Welcome to Jordan, Jordan is a nice country; it's amazing.";
 	
-	vector <string> S2 = ReplaceStringWithNoMatchCase(S1, StringToReplace, ReplaceTo);
-	string Result = JoinString(S2, " ");
+	cout << "Original String: \n" << S1 << endl;
+
+	cout << "Punctuation Removed: \n" << RemovePunctuation(S1) << endl;
 	
-	cout << "\n\n Replace with No Match Case: \n";
-	cout << Result << endl;
 	system("pause>0");
 }
