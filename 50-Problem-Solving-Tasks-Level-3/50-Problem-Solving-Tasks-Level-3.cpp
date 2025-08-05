5e904c088b9212ad31e2a114ac83c4249be79ee7
Emy1pa <souaouti.iman@gmail.com>
2025-08-05 10:29:52 +0100

    Solve Problem 45: Implement client data input and format it into a structured, savable string record.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -4,28 +4,47 @@
 
 using namespace std;
 
-string RemovePunctuation(string S1){
-	string Result = "";
-	for (int i = 0; i < S1.length();)
-	{
-		if (ispunct(S1[i])) {
-			S1.erase(i, 1);
-		}
-		else {
-			i++;
-		}
-	}
-	return S1;
+struct stClientData {
+	string AccountNumber = "";
+	string PinCode = "";
+	string ClientName = "";
+	string Phone = "";
+	int AccountBalance = 0;
+};
+
+stClientData ReadClientData(){
+	stClientData ClientData;
+	cout << "Enter your Account Number ? \n";
+	cin >> ClientData.AccountNumber;
+	cout << "Enter your PinCode ? \n";
+	cin >> ClientData.PinCode;
+	cout << "Enter your Name ? \n";
+	cin.ignore(1, '\n');
+	getline(cin, ClientData.ClientName);
+	cout << "Enter your Phone ? \n";
+	cin >> ClientData.Phone;
+	cout << "Enter your Account Balance ? \n";
+	cin >> ClientData.AccountBalance;
+	return ClientData;
 }
 
+string ConvertRecordToLine(stClientData ClientData, string Seperator = "#//#") {
+	string stClientRecord = "";
+	stClientRecord += ClientData.AccountNumber + Seperator;
+	stClientRecord += ClientData.PinCode + Seperator;
+	stClientRecord += ClientData.ClientName + Seperator;
+	stClientRecord += ClientData.Phone + Seperator;
+	stClientRecord += to_string(ClientData.AccountBalance);
+	return stClientRecord;
+}
 
 int main()
 {
-	string S1 = "Welcome to Jordan, Jordan is a nice country; it's amazing.";
-	
-	cout << "Original String: \n" << S1 << endl;
+	stClientData ClientData;
+	cout << "Please enter Client Data: \n\n";
+	ClientData = ReadClientData();
 
-	cout << "Punctuation Removed: \n" << RemovePunctuation(S1) << endl;
-	
+	cout << "Client Record for saving is: \n";
+	cout << ConvertRecordToLine(ClientData);
 	system("pause>0");
 }
