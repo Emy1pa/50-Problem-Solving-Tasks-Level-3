3a68e06dbcbc30300ac1e7ceb8448649eb480236
Emy1pa <souaouti.iman@gmail.com>
2025-08-06 14:27:19 +0100

    Solve Problem 48: Read client records from file, parse lines, store in vector, and display formatted table with account, name, balance.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <vector>
 #include <fstream>
-
+#include <iomanip>
 
 using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+
 
 struct stClientData {
 	string AccountNumber = "";
@@ -14,62 +17,72 @@ struct stClientData {
 	int AccountBalance = 0;
 };
 
-stClientData ReadClientData() {
+vector <string> SplitString(string Line, string Seperator){
+	int pos = 0;
+	string sWord = "";
+	vector <string> vClient;
+	while((pos = Line.find(Seperator)) != std::string::npos){
+		sWord = Line.substr(0, pos);
+		if (sWord != "") {
+			vClient.push_back(sWord);
+		}
+		Line.erase(0, pos + Seperator.length());
+	}
+	if (Line != "") {
+		vClient.push_back(Line);
+	}
+	return vClient;
+}
+
+stClientData ConvertLineToRecord(string Line) {
 	stClientData ClientData;
-	cout << "Enter Account Number? ";
-	cin.ignore(1, '/n');
-	getline(cin, ClientData.AccountNumber);
-	cout << "Enter PinCode? ";
-	getline(cin, ClientData.PinCode);
-	cout << "Enter Name? ";
-	getline(cin, ClientData.ClientName);
-	cout << "Enter Phone? ";
-	getline(cin, ClientData.Phone);
-	cout << "Enter Account Balance? ";
-	cin >> ClientData.AccountBalance;
+	vector <string> vClientData = SplitString(Line, "#//#");
+	ClientData.AccountNumber = vClientData[0];
+	ClientData.PinCode = vClientData[1];
+	ClientData.ClientName = vClientData[2];
+	ClientData.Phone = vClientData[3];
+	ClientData.AccountBalance =stod(vClientData[4]);
 	return ClientData;
 }
 
-string ConvertRecordToLine(stClientData ClientData, string Seperator = "#//#") {
-	string RecordLine = "";
-	RecordLine += ClientData.AccountNumber + Seperator;
-	RecordLine += ClientData.PinCode + Seperator;
-	RecordLine += ClientData.ClientName + Seperator;
-	RecordLine += ClientData.Phone + Seperator;
-	RecordLine += to_string(ClientData.AccountBalance);
-	return RecordLine;
-}
-
-void AddStringToFile(string Line){
+vector <stClientData> ReadFileContent() {
+	vector <stClientData> vClients;
 
 	fstream MyFile;
 
-	MyFile.open("ClientFile", ios::app);
+	MyFile.open(ClientsFileName, ios::in);
 	if (MyFile.is_open()) {
-		MyFile << Line << endl;
+		string Line;
+		while (getline(MyFile, Line)) {
+			if (Line != "") {
+				stClientData Client = ConvertLineToRecord(Line);
+				vClients.push_back(Client);
+			}
+		}
+		MyFile.close();
 	}
-	MyFile.close();
+	return vClients;
 }
 
-void KeepAddingClients(){
-	char AddMore = 'Y';
-	do
-	{
-		stClientData ClientData = ReadClientData();
-		string ConvertToLine = ConvertRecordToLine(ClientData);
-		AddStringToFile(ConvertToLine);
-		cout << "Client Added Successfully, do you want to add more clients ? ";
-		cin >> AddMore;
-	} while (AddMore == 'Y' || AddMore == 'y');
+void DisplayClientData(vector <stClientData> ClientData){
+	cout << "\n-----------------------------------------------------------------------------------------------------------------------\n";
+	cout << "| " << setw(17) << "Account Number" << " | " << setw(11) << "PinCode" << " | " << setw(34) << "Client Name " << " | " << setw(13) << "Phone " << " | " << setw(20) << "Balance " << endl;
+	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
+	for (stClientData& Client : ClientData) {
+	cout << "| " << setw(17) << Client.AccountNumber << " | " << setw(11) << Client.PinCode << " | " << setw(34) << Client.ClientName << " | " << setw(13) << Client.Phone << " | " << setw(20) << Client.AccountBalance << endl;
+	}
+	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
 }
 
 
+void DisplayFinalResults(){
+	vector <stClientData> ClientData = ReadFileContent();
+	DisplayClientData(ClientData);
 
+}
 
 int main()
 {
-	
-	cout << "Adding New Client: \n\n";
-	KeepAddingClients();
+	DisplayFinalResults();
 	system("pause>0");
 }
