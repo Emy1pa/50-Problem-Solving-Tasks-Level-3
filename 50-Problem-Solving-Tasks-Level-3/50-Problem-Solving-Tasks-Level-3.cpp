9b2549184ad8d891432cabd5ecf25c01323049bd
Emy1pa <souaouti.iman@gmail.com>
2025-08-06 15:37:13 +0100

    Solve Problem 49: Add search by account number feature with client details display.

--- 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
+++ 50-Problem-Solving-Tasks-Level-3/50-Problem-Solving-Tasks-Level-3.cpp
@@ -64,26 +64,50 @@ vector <stClientData> ReadFileContent() {
 	return vClients;
 }
 
-void DisplayClientData(vector <stClientData> ClientData){
-	cout << "\n-----------------------------------------------------------------------------------------------------------------------\n";
-	cout << "| " << setw(17) << "Account Number" << " | " << setw(11) << "PinCode" << " | " << setw(34) << "Client Name " << " | " << setw(13) << "Phone " << " | " << setw(20) << "Balance " << endl;
-	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
-	for (stClientData& Client : ClientData) {
-	cout << "| " << setw(17) << Client.AccountNumber << " | " << setw(11) << Client.PinCode << " | " << setw(34) << Client.ClientName << " | " << setw(13) << Client.Phone << " | " << setw(20) << Client.AccountBalance << endl;
+//void DisplayClientData(vector <stClientData> ClientData){
+//	cout << "\n-----------------------------------------------------------------------------------------------------------------------\n";
+//	cout << "| " << left << setw(17) << "Account Number" << " | "  << left << setw(11) << "PinCode" << " | " << left << setw(34) << "Client Name " << " | " << left << setw(13) << "Phone " << " | " << left << setw(20) << "Balance " << endl;
+//	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
+//	for (stClientData& Client : ClientData) {
+//	cout << "| " << left << setw(17) << Client.AccountNumber << " | " << left << setw(11) << Client.PinCode << " | " << left << setw(34) << Client.ClientName << " | " << left << setw(13) << Client.Phone << " | " << left << setw(20) << Client.AccountBalance << endl;
+//	}
+//	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
+//}
+
+//void DisplayFinalResults(){
+//	vector <stClientData> ClientData = ReadFileContent();
+//	cout << "\t\t\t\t\tClient List (" << ClientData.size() << ") Client(s). \n";
+//	DisplayClientData(ClientData);
+//
+//}
+
+void DisplayClientData(stClientData ClientData) {
+	cout << "The following are the client details: \n";
+	cout << "Account Number: " << ClientData.AccountNumber << endl;
+	cout << "PinCode: " << ClientData.PinCode << endl;
+	cout << "Client Name: " << ClientData.ClientName << endl;
+	cout << "Client Phone: " << ClientData.Phone << endl;
+	cout << "Account Balance: " << ClientData.AccountBalance << endl;
+}
+
+bool SearchForClientAccount() {
+	string AccountNumber;
+	cout << "Please enter Account Number ? \n";
+	cin >> AccountNumber;
+	vector <stClientData> vClients = ReadFileContent();
+	for (stClientData& Client : vClients) {
+		if (AccountNumber == Client.AccountNumber) {
+			DisplayClientData(Client);
+			return true;
+		}
 	}
-	cout << "-----------------------------------------------------------------------------------------------------------------------\n";
+	cout << "Client with Account Number (" << AccountNumber << ") Not Found!\n";
+	return false;
 }
 
-
-void DisplayFinalResults(){
-	vector <stClientData> ClientData = ReadFileContent();
-	cout << "\t\t\t\t\tClient List (" << ClientData.size() << ") Client(s). \n";
-	DisplayClientData(ClientData);
-
-}
 
 int main()
 {
-	DisplayFinalResults();
+	SearchForClientAccount(); 
 	system("pause>0");
 }
